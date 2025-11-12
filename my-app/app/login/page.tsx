5a818a9d9eb5079ed918c8c7b77102dfc613533e
prachi-achari <prachiachari06@gmail.com>
2025-11-12 09:36:39 +0530

    Update page.tsx

--- my-app/app/login/page.tsx
+++ my-app/app/login/page.tsx
@@ -5,8 +5,7 @@ import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
       <LoginForm />
-    </div>
   );
 }
+
